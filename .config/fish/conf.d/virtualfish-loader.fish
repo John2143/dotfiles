a8b22a86d12139299581003654f72c11a6afb436
John Schmidt <john@john2143.com>
2021-04-13 17:36:03 -0400

    Modified files

--- .config/fish/conf.d/virtualfish-loader.fish
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -1,3 +1,7 @@
+if not test -e /usr/local/lib/python3.9/site-packages/virtualfish/virtual.fish
+    exit 0
+end
+
 set -g VIRTUALFISH_VERSION 2.5.1
 set -g VIRTUALFISH_PYTHON_EXEC /usr/local/opt/python@3.9/bin/python3.9
 source /usr/local/lib/python3.9/site-packages/virtualfish/virtual.fish
