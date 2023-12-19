e69609ef89014fcaef8ab1de4aaf4cd2168c546d
John Schmidt <john@john2143.com>
2023-12-19 10:13:38 -0500

    Modified files

--- .config/startup.fish
+++ .config/startup.fish
@@ -1,31 +1,36 @@
 #!/usr/bin/env fish
-sleep 0.1
-tmux new-window '/usr/bin/monerod --block-sync-size 100 --data-dir /mnt/other/monero/ --max-concurrency 4 --db-sync-mode=safe:sync'
-tmux rename-window 'monero'
 
-sleep 0.1
-tmux new-window 'sudo openrgb'
-tmux rename-window 'openrgb'
-
-sleep 0.1
-tmux new-window 'sudo input-remapper-gtk'
-tmux rename-window 'inputs'
+set HOST (cat /etc/hostname)
 
 sleep 0.1
 tmux new-window 'steam'
 tmux rename-window 'steam'
 
 sleep 0.1
-tmux new-window 'spotify-launcher'
-tmux rename-window 'spotify'
-
-sleep 0.1
 tmux new-window 'obsidian'
 tmux rename-window 'obsidian'
 
-sleep 0.1
-tmux new-window 'ts3'
-tmux rename-window 'ts3'
+if test "$HOST" = "arch"
+    sleep 0.1
+    tmux new-window 'teamspeak3'
+    tmux rename-window 'teamspeak3'
+
+    sleep 0.1
+    tmux new-window '/usr/bin/monerod --block-sync-size 100 --data-dir /mnt/other/monero/ --max-concurrency 4 --db-sync-mode=safe:sync'
+    tmux rename-window 'monero'
+
+    sleep 0.1
+    tmux new-window 'sudo openrgb'
+    tmux rename-window 'openrgb'
+
+    sleep 0.1
+    tmux new-window 'sudo input-remapper-gtk'
+    tmux rename-window 'inputs'
+
+    sleep 0.1
+    tmux new-window 'spotify-launcher'
+    tmux rename-window 'spotify'
+end
 
 sleep 0.1
 tmux new-window 'discord'
--- .xprofile.fish
+++ .xprofile.fish
@@ -1,11 +1,10 @@
 #!/usr/bin/env fish
 set HOST (cat /etc/hostname)
 
+# Set up monitorss
 if test "$HOST" = "downstairs"
     xrandr --output DisplayPort-1 --mode 2560x1440 --rate 239.97 --primary
     xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144.00 --right-of DisplayPort-1
-    xrandr --output HDMI-A-0 --off
-    xrandr --output HDMI-A-1 --off
 
     sudo ip link set dev enp6s0 up
     sudo ip addr add 192.168.1.9/24 dev enp6s0
@@ -14,35 +13,34 @@ if test "$HOST" = "downstairs"
 
     killall spotifyd || true
     spotifyd -p $SPOTIFY_PASSWORD -u $SPOTIFY_USERNAME --device-name downstairs --device-type computer --bitrate 320 --backend pulseaudio &
-else # arch
+end
+
+if test "$HOST" = "arch"
     xrandr --output DP-4 --mode 2560x1440 --rate 144.0 --primary
     xrandr --output DP-0 --mode 2560x1440 --rate 144.0 --right-of DP-4
     xrandr --output DP-2 --off
 end
 
+sleep .1
+
+echo "power on" > bluetoothctl
+
+#BSPC config
+#
 setxkbmap -option "ctrl:nocaps"
 dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
 kdeconnect-cli --refresh
-
 killall polybar
-killall sxhkd
 
-sleep .1
 
-if test "$HOST" = "downstairs"
-    polybar -r 3 &
-    polybar -r 4 &
-else # arch
-    polybar -r 1 &
-    polybar -r 2 &
-end
+# killall swhks || true
+# sudo killall xwhkd || true
+# swhks &
 
+killall sxhkd || true
 sxhkd &
-#killall nitrogen
-#nitrogen --restore &
 
-# how long until hibernate, screen off, etc
-xset dpms 6000 6000 12000
+# sudo pkexec fish -c "set -x XDG_CONFIG_HOME '$XDG_CONFIG_HOME'; set -x XDG_RUNTIME_DIR '$XDG_RUNTIME_DIR'; "(which swhkd)
 
 bspc config border_width         0
 bspc config window_gap           1
@@ -67,20 +65,27 @@ bspc rule -a discord desktop='disc'
 bspc rule -a obsidian desktop='obsidian'
 bspc rule -a "TeamSpeak 3" desktop='ts'
 
-if test "$HOST" = "downstairs"
-    bspc monitor DisplayPort-1 -d A1 A2 A3 A4 A5 A6 A7 A8 A9
-    bspc monitor DisplayPort-0 -d B1 B2 B3 B4 B5 B6 B7 ts disc steam obsidian
-else
+if test "$HOST" = "arch"
+    xset dpms 6000 6000 12000
+
+    polybar -r 1 &
+    polybar -r 2 &
+
+
     bspc monitor DP-4 -d A1 A2 A3 A4 A5 A6 A7 A8 A9
     bspc monitor DP-0 -d B1 B2 B3 B4 B5 B6 B7 ts spotify disc steam obsidian
-end
 
-sleep 1
-
-if test "$HOST" = "arch"
     sudo systemctl restart dnscrypt-proxy
     sudo modprobe i2c-dev
     sudo modprobe i2c-i801
+end
+
+if test "$HOST" = "downstairs"
+    polybar -r 3 &
+    polybar -r 4 &
+
+    bspc monitor DisplayPort-1 -d A1 A2 A3 A4 A5 A6 A7 A8 A9
+    bspc monitor DisplayPort-0 -d B1 B2 B3 B4 B5 B6 B7 ts disc steam obsidian
 end
 
 notify-send "Desktop ready"
