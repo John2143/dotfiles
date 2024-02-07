01e05eb48da9c0a3f7b635972a1ca365775f6d23
John Schmidt <john_schmidt@discovery.com>
2024-02-07 12:09:13 -0500

    improve awsctx

--- awsctx/src/main.rs
+++ awsctx/src/main.rs
@@ -1,7 +1,13 @@
 #![feature(never_type)]
 
-use clap::Parser;
 use anyhow::bail;
+use clap::Parser;
+use nom::{
+    bytes::complete::{tag, take_while, take_until},
+    character::complete::{anychar, space0},
+    combinator::not,
+    sequence::delimited, Finish,
+};
 
 /// Simple program change environment varables for AWS
 #[derive(Parser, Debug)]
@@ -17,7 +23,7 @@ struct Args {
     env: Option<String>,
 }
 
-use std::{collections::HashMap, process::Stdio, str::from_utf8, io::Read};
+use std::{collections::HashMap, io::Read, process::Stdio, str::from_utf8};
 
 use tokio::{io::AsyncWriteExt, process::Command};
 
@@ -27,6 +33,37 @@ struct Cred {
     aws_secret_access_key: Option<String>,
     aws_session_token: Option<String>,
     aws_security_token: Option<String>,
+    x_security_token_expires: Option<String>,
+}
+
+use chrono::{DateTime, Duration, ParseError, TimeZone, Utc};
+impl Cred {
+    fn parse_date_string(date_str: &str) -> Result<DateTime<Utc>, ParseError> {
+        let format = "%Y-%m-%dT%H:%M:%S%z";
+        Utc.datetime_from_str(date_str, format)
+    }
+
+    fn is_expired(&self) -> Option<Duration> {
+        self.x_security_token_expires
+            .as_deref()
+            .map(Self::parse_date_string)
+            .map(|x| x.expect("Invalid Datestring in Credentials"))
+            .map(|x| {
+                let now = Utc::now();
+                if x > now {
+                    Some(x - now)
+                } else {
+                    None
+                }
+            })
+            .flatten()
+    }
+}
+
+fn format_duration(duration: Duration) -> String {
+    let minutes = (duration.num_seconds() / 60) % 60;
+    let hours = (duration.num_seconds() / 60) / 60;
+    format!("{}h{}m", hours, minutes)
 }
 
 enum ShellOutput {
@@ -78,9 +115,9 @@ async fn rm() -> anyhow::Result<()> {
     let profile_to_check = match cli.env {
         Some(s) if s == "-" => {
             let mut env = Default::default();
-            std::io::stdin().read_to_end(&mut env);
+            std::io::stdin().read_to_end(&mut env)?;
             String::from_utf8(env)?
-        },
+        }
         Some(env) => env,
         // If we don't supply env as a flag, launch fzf
         None => {
@@ -100,7 +137,16 @@ async fn rm() -> anyhow::Result<()> {
                     continue;
                 }
 
-                let fzf_line = format!("{group}\n");
+                let fzf_line = match creds.is_expired() {
+                    Some(x) => {
+                        let dur = format_duration(x);
+                        format!("[{dur}] {group}\n")
+                    }
+                    None => {
+                        format!("[Expired] {group}\n")
+                    }
+                };
+
                 cmd_stdin.write_all(fzf_line.as_bytes()).await?;
             }
 
@@ -111,13 +157,20 @@ async fn rm() -> anyhow::Result<()> {
                 return Err(anyhow::Error::msg("Failed to run fzf"));
             }
 
-            String::from_utf8(fzf_output.stdout)?
-        },
+            let cmd_out = from_utf8(&fzf_output.stdout)?;
+            //remove the `[expired]` from the front of the word
+            let cmd_out = run_drop_tags(cmd_out)?;
+            cmd_out.to_string()
+        }
     };
 
     let profile = match s.get(profile_to_check.trim()) {
         Some(s) => s,
-        None => bail!("Profile {} does not exist in {}", profile_to_check, CRED_LOC),
+        None => bail!(
+            "Profile {} does not exist in {}",
+            profile_to_check,
+            CRED_LOC
+        ),
     };
     let shell: ShellOutput = std::env::var("SHELL")
         .map(|x| x.parse().unwrap())
@@ -150,4 +203,28 @@ async fn main() -> Result<(), String> {
         Ok(_) => Ok(()),
         Err(e) => Err(e.to_string()),
     }
+}
+
+fn drop_tags(input: &str) -> nom::IResult<&str, ()> {
+    let (input, _) = delimited(tag("["), take_until("]"), tag("]"))(input)?;
+    let (input, _) = space0(input)?;
+    Ok((input, ()))
+}
+
+/// This removes brackets from a string. ex: `[asdf] 123456` -> `123456`
+fn run_drop_tags(input: &str) -> anyhow::Result<&str> {
+    let s = match drop_tags(input).finish() {
+        Ok(s) => s.0,
+        // We added the bracket on at the start, idk how this would happen
+        Err(_) => panic!("Couldn't remove the brackets from the input string to fzf"),
+    };
+
+    Ok(s)
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn try_tag() {}
 }
