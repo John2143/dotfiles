c7d45174b52067f5fe55163d76ace9be725aa320
John Schmidt <john@john2143.com>
2024-05-17 12:50:56 -0400

    Modified files

--- .xprofile.fish
+++ .xprofile.fish
@@ -9,26 +9,6 @@ set brightness_day 1.0
 set brightness_night 0.75
 set loc "38.897916:-77.035476"
 
-# Set up monitorss
-if test "$HOST" = "downstairs"
-    set left_mon "DP-1"
-    set right_mon "DP-2"
-    xrandr --output $left_mon --mode 2560x1440 --rate 239.97 --primary
-    xrandr --output $right_mon --mode 1920x1080 --rate 144.00 --right-of $left_mon
-
-    # WAN
-    #sudo ip link set dev enp6s0 up
-    #sudo ip addr add 192.168.1.9/24 dev enp6s0
-    #sudo ip ro add 192.168.1.0/24 dev enp6s0
-    #sudo ip ro add 0.0.0.0/0 via 192.168.1.1
-
-    # WIFI
-    #sudo ip link set dev wlp2s0f0u3 up
-    #sudo ip addr add 192.168.1.9/24 dev wlp2s0f0u3
-    #sudo ip ro add 192.168.1.0/24 dev wlp2s0f0u3
-    #sudo ip ro add 0.0.0.0/0 via 192.168.1.1
-end
-
 if test "$HOST" = "arch"
     set left_mon "DP-0"
     set right_mon "HDMI-1"
@@ -94,11 +74,29 @@ if test "$HOST" = "arch"
 end
 
 if test "$HOST" = "office"
+    # TODO setup monitors
+    #set left_mon "DP-1"
+    #set right_mon "DP-2"
+    #xrandr --output $left_mon --mode 2560x1440 --rate 239.97 --primary
+    #xrandr --output $right_mon --mode 1920x1080 --rate 144.00 --right-of $left_mon
     killall spotifyd || true
     spotifyd -p $SPOTIFY_PASSWORD -u $SPOTIFY_USERNAME --device-name office --device-type computer --bitrate 320 --backend pulseaudio &
 
     killall gammastep || true
     gammastep -l $loc -b $brightness_day:$brightness_night -t $color_temp_day:$color_temp_night &
+end
+if test "$HOST" = "downstairs"
+    # WAN
+    #sudo ip link set dev enp6s0 up
+    #sudo ip addr add 192.168.1.9/24 dev enp6s0
+    #sudo ip ro add 192.168.1.0/24 dev enp6s0
+    #sudo ip ro add 0.0.0.0/0 via 192.168.1.1
+
+    # WIFI
+    #sudo ip link set dev wlp2s0f0u3 up
+    #sudo ip addr add 192.168.1.9/24 dev wlp2s0f0u3
+    #sudo ip ro add 192.168.1.0/24 dev wlp2s0f0u3
+    #sudo ip ro add 0.0.0.0/0 via 192.168.1.1
 end
 
 notify-send "Desktop ready"
