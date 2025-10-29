3e978916b03f899c895d61174991e59f020730cd
John Schmidt <john@john2143.com>
2025-10-29 04:43:44 -0400

    reconfigure drives

--- nixos/arch-hardware-configuration.nix
+++ nixos/arch-hardware-configuration.nix
@@ -35,22 +35,40 @@
     neededForBoot = true;
   };
 
-  fileSystems."/home/john" = {
-    # this is a bind mount: mount --bind /mnt/other/home/john/john/ /home/john
-    device = "/mnt/other/home/john/john";
-    fsType = "none";
-    options = [ "bind" ];
-    neededForBoot = true;
-  };
-
   #fileSystems."/mnt/arch" = {
     #device = "/dev/disk/by-label/ARCH";
     #fsType = "ext4";
   #};
 
-  fileSystems."/mnt/other" = {
-    device = "/dev/disk/by-label/OTHER";
+  #fileSystems."/mnt/other" = {
+    #device = "/dev/disk/by-label/OTHER";
+    #fsType = "ext4";
+    #neededForBoot = true;
+  #};
+
+  fileSystems."/mnt/x9" = {
+    device = "/dev/disk/by-label/x9";
     fsType = "ext4";
+    neededForBoot = true;
+  };
+
+  fileSystems."/mnt/other" = {
+    device = "/mnt/x9/other";
+    fsType = "none";
+    options = [ "bind" ];
+    neededForBoot = true;
+  };
+
+  #fileSystems."/home/john" = {
+    #device = "/dev/pool1a/john_home";
+    #fsType = "ext4";
+    #neededForBoot = true;
+  #};
+
+  fileSystems."/home/john" = {
+    device = "/mnt/x9/other/home/john/john";
+    fsType = "none";
+    options = [ "bind" ];
     neededForBoot = true;
   };
 
