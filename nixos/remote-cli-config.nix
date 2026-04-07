25137feb0e405ddac1dc183f5f3aad7d3f78038b
John Schmidt <john@john2143.com>
2026-04-07 16:55:52 -0400

    update all

---
+++ nixos/remote-cli-config.nix
@@ -0,0 +1,118 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{
+  config,
+  lib,
+  pkgs,
+  pkgs-stable,
+  inputs,
+  compName,
+  sshKeys,
+  ...
+}:
+
+{
+  imports = [
+    ./security-hardware-configuration.nix
+    ./modules/user-john.nix
+    #./modules/ollama.nix
+    # inputs.home-manager.nixosModules.default
+  ];
+  home-manager.users."john" = import ./home-cli.nix;
+  services.getty.autologinUser = "john";
+
+  # Use the systemd-boot EFI boot loader.;
+  boot.loader = {
+    grub.enable = false;
+    generic-extlinux-compatible.enable = true;
+    #systemd-boot.enable = true;
+  };
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
+  services.resolved = {
+    enable = true;
+    settings = {
+      Resolve = {
+        DNSOverTLS = "true";
+        DNSSEC = "true";
+        Domains = [
+          "~."
+        ];
+        FallbackDNS = [
+          "1.1.1.1"
+          "1.0.0.1"
+        ];
+      };
+    };
+  };
+
+  networking.hostName = compName; # Define your hostname.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  networking.nameservers = [
+    "1.1.1.1"
+  ];
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    fish
+    curl
+  ];
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  programs.fish.enable = true;
+
+  # ================
+  # === Services ===
+  # ================
+
+  services.openssh.enable = true;
+
+  # Enable the OpenSSH daemon.
+  users.users."john".openssh.authorizedKeys.keys = sshKeys;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
+  security.rtkit.enable = true;
+
+  # networking.firewall.allowedTCPPorts = [
+  #   5353 # avahi
+  #   7777 # games
+  # ];
+  # networking.firewall.allowedUDPPorts = [  ];
+  #networking.firewall.enable = true;
+
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+}
