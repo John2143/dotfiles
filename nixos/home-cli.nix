f3f00dd7ca6f2f7eb7b6d2bb96ec91d943dd7900
John Schmidt <john@john2143.com>
2026-04-13 10:21:12 -0400

    temp

--- nixos/home-cli.nix
+++ nixos/home-cli.nix
@@ -175,15 +175,15 @@ in
         set -f nodes (tailscale status --json | jq -r '.Peer[] | select(.ExitNodeOption == true) | .DNSName' | string collect)
         # add a node of "None" to the list of nodes so that we can exit if the user doesn't select a node
         # and also keep newlines separating the nodes so that fzf can display them on separate lines
-        set nodes "None\n$nodes"
+        set nodes "None"\n"$nodes"
         set -f new_node (echo $nodes | fzf)
         if test "A$new_node" = "A"
             exit 1
         end
         if test $new_node = "None"
-            tailscale up --exit-node=""
+            tailscale set --exit-node=""
         else
-            tailscale up --exit-node=$new_node --exit-node-allow-lan-access
+            tailscale set --exit-node=$new_node --exit-node-allow-lan-access
         end
       '';
       vc.description = "Select a Tailscale exit node";
