354db05099ec675d7b77fe9d3933b810958a29a7
John Schmidt <john@john2143.com>
2026-04-13 10:14:48 -0400

    exit nodes

--- nixos/home-cli.nix
+++ nixos/home-cli.nix
@@ -172,9 +172,10 @@ in
         kubectl config use-context $new_env
       '';
       vc.body = ''
-        set -f nodes (tailscale status --json | jq -r '.Peer[] | select(.ExitNodeOption == true) | .DNSName')
+        set -f nodes (tailscale status --json | jq -r '.Peer[] | select(.ExitNodeOption == true) | .DNSName' | string collect)
         # add a node of "None" to the list of nodes so that we can exit if the user doesn't select a node
-        set nodes $nodes None
+        # and also keep newlines separating the nodes so that fzf can display them on separate lines
+        set nodes "None\n$nodes"
         set -f new_node (echo $nodes | fzf)
         if test "A$new_node" = "A"
             exit 1
