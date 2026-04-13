a4a8562d5700513811321f884ea6d01f9856f486
John Schmidt <john@john2143.com>
2026-04-13 01:58:53 -0400

    add vpin

--- flake.nix
+++ flake.nix
@@ -154,6 +154,23 @@
         ];
       };
 
+      nixosConfigurations.vpin = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit inputs;
+          compName = "vpin";
+          sshKeys = my-keys;
+        };
+        modules = [
+          inputs.home-manager.nixosModules.default
+          ./nixos/shared-cli-configuration.nix
+          ./nixos/remote-cli-config.nix
+          #./nixos/modules/k3s-agent.nix
+          ./nixos/tailscale.nix
+        ];
+      };
+
+
       nixosConfigurations.term = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = {
