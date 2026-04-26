3864286f655b9796ed88de519c5227cfd41fddb7
John Schmidt <john@john2143.com>
2026-04-25 22:15:10 -0400

    Add groups fixes for ollama

--- nixos/modules/ollama.nix
+++ nixos/modules/ollama.nix
@@ -12,4 +12,14 @@
     host = "0.0.0.0";
     openFirewall = true;
   };
+
+  # NAS CIFS mounts (modules/nas-mounts.nix) use uid=1000/gid=1000 so only `john` can write.
+  # The stock ollama unit uses DynamicUser with a different UID, which breaks pulls to
+  # /mnt/nas/share/... with "permission denied".
+  systemd.services.ollama.serviceConfig = {
+    User = lib.mkForce "john";
+    Group = lib.mkForce "users";
+    DynamicUser = lib.mkForce false;
+    PrivateUsers = lib.mkForce false;
+  };
 }
