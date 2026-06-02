72a1dc1addf4436468cccd4d19f03bc3b55188c9
John Schmidt <john@john2143.com>
2026-06-02 14:07:10 -0400

    remove old programS

--- .config/startup.fish
+++ .config/startup.fish
@@ -1,8 +1,6 @@
 #!/usr/bin/env fish
 
-
 set HOST (cat /etc/hostname)
-
 
 if tmux has-session -t monitor 2>/dev/null
     exit
@@ -13,20 +11,8 @@ tmux new-window 'btop'
 tmux rename-window 'btop'
 
 sleep 0.1
-tmux new-window 'while true; steam; end'
+tmux new-window 'steam'
 tmux rename-window 'steam'
-
-sleep 0.1
-tmux new-window 'flatpak run md.obsidian.Obsidian'
-tmux rename-window 'obsidian'
-
-sleep 0.1
-tmux new-window 'flatpak run dev.vencord.Vesktop'
-tmux rename-window 'discord'
-
-sleep 0.1
-tmux new-window 'flatpak run com.spotify.Client'
-tmux rename-window 'spotify'
 
 if test "$HOST" = "arch"
     sleep 0.1
@@ -35,10 +21,6 @@ if test "$HOST" = "arch"
 end
 
 if test "$HOST" = "office"
-    sleep 0.1
-    tmux new-window 'amdgpu_top'
-    tmux rename-window 'gpu top'
-
     sleep 0.1
     tmux new-window 'qpwgraph'
     tmux rename-window 'qpwgraph'
