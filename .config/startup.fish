dd5e6e716e6b5ef79453da0e66f395f365bdedf9
John Schmidt <john@john2143.com>
2026-06-02 14:05:55 -0400

    change startup

--- .config/startup.fish
+++ .config/startup.fish
@@ -1,6 +1,12 @@
 #!/usr/bin/env fish
 
+
 set HOST (cat /etc/hostname)
+
+
+if tmux has-session -t monitor 2>/dev/null
+    exit
+end
 
 sleep 0.1
 tmux new-window 'btop'
@@ -23,25 +29,9 @@ tmux new-window 'flatpak run com.spotify.Client'
 tmux rename-window 'spotify'
 
 if test "$HOST" = "arch"
-    #sleep 0.1
-    #tmux new-window 'while true; ts3client; read; end'
-    #tmux rename-window 'ts3client'
-
     sleep 0.1
     tmux new-window 'monerod --block-sync-size 100 --data-dir /mnt/monero/monero/ --max-concurrency 4 --db-sync-mode=safe:sync'
     tmux rename-window 'monero'
-
-    #sleep 0.1
-    #tmux new-window 'sudo openrgb'
-    #tmux rename-window 'openrgb'
-
-    #sleep 0.1
-    #tmux new-window 'sudo input-remapper-gtk'
-    #tmux rename-window 'inputs'
-
-    #sleep 0.1
-    #tmux new-window 'spotify-launcher'
-    #tmux rename-window 'spotify'
 end
 
 if test "$HOST" = "office"
