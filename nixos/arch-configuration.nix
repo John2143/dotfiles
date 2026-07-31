fb898bd7f43fe9f10afa0547d48ab1cd0c22a8b8
John Schmidt <john@john2143.com>
2026-07-31 15:21:13 -0400

    fix(arch): uncordon, remove seated NoSchedule taint

--- nixos/arch-configuration.nix
+++ nixos/arch-configuration.nix
@@ -102,7 +102,6 @@
   systemd.services.openrgb.enable = lib.mkForce false;
 
 
-  custom.k3sNodeTaints = ["seated=true:NoSchedule"];
   custom.k3sStorageAfter = ["mnt-longhorn.mount"];
 
   # k3s server — join existing cluster via mDNS (bootstrap without tailscale dependency).
